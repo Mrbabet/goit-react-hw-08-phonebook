20702452913a2cd15f3e62425547efae298c179c
Mrbabet <wieliczkokamil@gmail.com>
2024-02-26 21:27:15 +0100

    added 404

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/goit-react-hw-08-phonebook/",
+  server: {
+    // Handle fallback for history API routing
+    historyApiFallback: true,
+  },
 });
